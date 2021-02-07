50f8c6285f81b167315a3cc1a9269cc8809c9efb
like <980650920@qq.com>
2021-02-07 18:32:51 +0800

    对口后端接口

--- front/js/app.js
+++ front/js/app.js
@@ -1,4 +1,4 @@
-let environment = window.location.hostname === 'localhost' ? 'dev' : 'prod';
+let environment = 'dev';
 let baseUrl = environment === 'dev' ? 'http://localhost:8088' : 'http://47.105.41.229:80/api/mall-api';
 let shopIndexUrl = environment === 'dev' ? 'http://localhost:8080/mall-shop' : 'http://47.105.41.229:80/'
 
