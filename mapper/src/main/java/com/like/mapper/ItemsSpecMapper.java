7d299bccde07614b65de53eb43a6de5914a1ef37
like <980650920@qq.com>
2021-02-19 18:41:38 +0800

    订单扣除库存-乐观锁实现

--- mapper/src/main/java/com/like/mapper/ItemsSpecMapper.java
+++ mapper/src/main/java/com/like/mapper/ItemsSpecMapper.java
@@ -3,7 +3,10 @@ package com.like.mapper;
 import com.like.my.mapper.MyMapper;
 import com.like.pojo.ItemsSpec;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ItemsSpecMapper extends MyMapper<ItemsSpec> {
+
+    int decreaseItemSpecStock(@Param("id") String id, @Param("buyCount") int buyCount);
 }
--- service/src/main/java/com/like/service/impl/ItemsSpecServiceImpl.java
+++ service/src/main/java/com/like/service/impl/ItemsSpecServiceImpl.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mapper.ItemsSpecMapper;
 import com.like.pojo.ItemsSpec;
 import com.like.service.ItemsSpecService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author like
@@ -14,8 +17,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ItemsSpecServiceImpl extends ServiceImpl<ItemsSpecMapper, ItemsSpec> implements ItemsSpecService {
 
-    @Override
-    public void decreaseItemSpecStock(String id, int buyCount) {
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
 
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void decreaseItemSpecStock(String id, int buyCount) {
+        // 防止超卖
+        // synchronized加锁、锁表、分布式锁(zookeeper,redis)
+        if (itemsSpecMapper.decreaseItemSpecStock(id, buyCount) != 1) {
+            throw new RuntimeException("订单创建失败");
+        }
     }
 }
