020ea036349e0fb5c4ceac77027e6def0d81c15c
like <980650920@qq.com>
2021-02-24 17:26:26 +0800

    用户中心：增加后端验证

--- api/src/main/java/com/like/controller/center/UserCenterController.java
+++ api/src/main/java/com/like/controller/center/UserCenterController.java
@@ -10,10 +10,15 @@ import com.like.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author like
@@ -34,10 +39,14 @@ public class UserCenterController {
     public HttpJSONResult queryUserInfo(
             @RequestParam String userId,
             @RequestBody UserCenterBo user,
+            BindingResult bindResult,
             HttpServletRequest req, HttpServletResponse reps) {
+        // 验证用户信息是否正确
+        if (bindResult.hasErrors()) {
+            return HttpJSONResult.errorMap(getErrors(bindResult));
+        }
+
         Users resUser = userCenterService.updateUseUserCenterBO(userId, user);
-
-
         // 保护用户隐私信息
         setNullProperty(resUser);
         CookieUtils.setCookie(req, reps,
@@ -45,6 +54,24 @@ public class UserCenterController {
                 JsonUtils.objectToJson(resUser), true);
         // TODO: 2021/2/24 后续会更改，增加token，整合到redis，分布式会话
         return HttpJSONResult.ok();
+    }
+
+    /**
+     * 得到后端校验错误
+     *
+     * @param bindingResult 绑定的结果
+     * @return {@link Map<String, String>}
+     */
+    private Map<String, String> getErrors(BindingResult bindingResult) {
+        Map<String, String> fieldMapErrorInfo = new HashMap<String, String>();
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            fieldMapErrorInfo.put(field, message);
+        }
+
+        return fieldMapErrorInfo;
     }
 
     private void setNullProperty(Users u) {
--- pojo/src/main/java/com/like/pojo/bo/center/UserCenterBo.java
+++ pojo/src/main/java/com/like/pojo/bo/center/UserCenterBo.java
@@ -1,6 +1,9 @@
 package com.like.pojo.bo.center;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
 
 /**
  * @author like
@@ -12,10 +15,18 @@ public class UserCenterBo {
     private String username;
     private String password;
     private String confirmPassword;
+    @NotBlank(message = "用户昵称不能为空")
+    @Length(max = 12, message = "Z用户昵称不能超过12位")
     private String nickname;
+    @NotBlank(message = "用户昵称不能为空")
+    @Length(max = 12, message = "Z用户昵称不能超过12位")
     private String realname;
-    private String mobile;
+    @Email
     private String email;
+    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$", message = "手机号格式不正确")
+    private String mobile;
+    @Min(value = 0, message = "性别选择不正确")
+    @Max(value = 2, message = "性别选择不正确")
     private String sex;
     private String birthday;
 }
