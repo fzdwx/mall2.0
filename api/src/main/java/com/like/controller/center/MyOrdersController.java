0975bed4714b7e20563dde1614c57037badece0e
like <980650920@qq.com>
2021-02-26 15:58:07 +0800

    添加对分页信息的检查

--- api/src/main/java/com/like/controller/center/MyOrdersController.java
+++ api/src/main/java/com/like/controller/center/MyOrdersController.java
@@ -1,10 +1,12 @@
 package com.like.controller.center;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.like.controller.BaseController;
 import com.like.pojo.vo.MyOrdersVo;
 import com.like.service.center.OrderCenterService;
 import com.like.utils.HttpJSONResult;
 import io.swagger.annotations.Api;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "我的订单", tags = "我的订单相关操作接口")
 @RestController
 @RequestMapping("myOrders")
-public class MyOrdersController {
+public class MyOrdersController extends BaseController {
     @Autowired
     OrderCenterService orderCenterService;
 
@@ -25,6 +27,17 @@ public class MyOrdersController {
                                         @PathVariable String orderStatus,
                                         @RequestParam Integer page,
                                         @RequestParam Integer pageSize) {
+
+        if (StringUtils.isBlank(userId)) {
+            return HttpJSONResult.errorMsg(null);
+        }
+        if (page == null) {
+            page = 1;
+        }
+        if (pageSize == null) {
+            pageSize = DEFAULT_PAGESIZE;
+        }
+
         IPage<MyOrdersVo> myOrders = orderCenterService.queryOrdersByUserIdAndOrderStatus(userId, orderStatus, page, pageSize);
         return HttpJSONResult.ok(myOrders);
     }
