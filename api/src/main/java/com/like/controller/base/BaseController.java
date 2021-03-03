f4e32ecaddf35b909dd3a429d57d38a94155454e
like <980650920@qq.com>
2021-03-03 17:51:49 +0800

    首页子分类信息前缀格式修改

--- api/src/main/java/com/like/controller/base/BaseController.java
+++ api/src/main/java/com/like/controller/base/BaseController.java
@@ -26,8 +26,10 @@ public class BaseController {
     public static String REDIS_KEY_CAROUSEL = "carousel";
     /** 网站首页一级分类保存在redis中的key */
     public static String REDIS_KEY_CATS = "cats";
-    /** 网站首页一级分类的子分类保存在redis中的key REDIS_KEY_SUB_CAT_PREFIX+":"+rootCatId */
-    public static String REDIS_KEY_SUB_CAT_PREFIX;
+    /** 网站首页一级分类的子分类保存在redis中的key REDIS_KEY_SHOP_CART_PREFIX+rootCatId */
+    public static String REDIS_KEY_SHOP_CART_PREFIX = "shopCart:";
+    /** 用户购物车保存在redis中的key REDIS_KEY_SUB_CAT_PREFIX+rootCatId */
+    public static String REDIS_KEY_SUB_CAT_PREFIX = "subCat:";
     /** 服务端口号 */
     @Value("${server.port}")
     private String port;
