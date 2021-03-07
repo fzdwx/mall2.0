a6fcb7d9b6845bb2be54367f8caa0b5e446cb09e
like <980650920@qq.com>
2021-03-07 14:34:52 +0800

    注销接口

--- sso/src/main/java/com/like/controller/SSOController.java
+++ sso/src/main/java/com/like/controller/SSOController.java
@@ -32,7 +32,7 @@ public class SSOController {
     public static String REDIS_USER_TOKEN_PREFIX = "userToken:";
     /** 用户全局门票 userTicket:ticket,userId */
     public static String REDIS_USER_TICKET_PREFIX = "userTicket:";
-    /** 临时门票 userTempTicket:tempTicket*/
+    /** 临时门票 userTempTicket:tempTicket */
     public static String REDIS_USER_TEMP_TICKET_PREFIX = "userTempTicket:";
     /** 用户保存在cookie中的key */
     public static final String COOKIE_FOODIE_USER_INFO_KEY = "user";
@@ -61,19 +61,23 @@ public class SSOController {
         return "login";
     }
 
-    private boolean verifyUserTicket(String userTicket) {
-        if (StringUtils.isBlank(userTicket)) {
-            return false;
-        }
-        String userId = redisUtil.get(REDIS_USER_TICKET_PREFIX + userTicket);
-        if (StringUtils.isBlank(userId)) {
-            return false;
-        }
-        String userInfo = redisUtil.get(REDIS_USER_TOKEN_PREFIX + userId);
-        if (StringUtils.isBlank(userInfo)) {
-            return false;
-        }
-        return true;
+    /**
+     * 注销
+     */
+    @PostMapping("/logout")
+    @ResponseBody
+    public HttpJSONResult logout(
+            @RequestParam String userId,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 1.获取cookie中用户cas的全局门票
+        String userTicket = CookieUtils.getCookieValue(request, COOKIE_USER_TICKET, true);
+        // 2.删除cookie中的用户全局门票
+        CookieUtils.deleteCookie(request, response, COOKIE_USER_TICKET);
+        // 3.删除用户redis中的用户全局门票信息
+        redisUtil.delete(REDIS_USER_TICKET_PREFIX + userTicket);
+        // 4.清除用户全局会话 token
+        redisUtil.delete(REDIS_USER_TOKEN_PREFIX + userId);
+        return HttpJSONResult.ok();
     }
 
     /**
@@ -117,6 +121,17 @@ public class SSOController {
         return "redirect:" + returnUrl + "?tempTicket=" + tempTicket;
     }
 
+    /**
+     * 验证临时门票
+     * - 用户验证为登录状态后会返回一个临时门票，并在redis中保存这个临时门票
+     * - 经过检验后，成功会在cookie保存用户信息
+     * - 失败就返回异常
+     * @param tempTicket 临时的机票
+     * @param request 请求
+     * @param response 响应
+     * @return {@link HttpJSONResult}
+     * @throws Exception 异常
+     */
     @PostMapping("/verifyTmpTicket")
     @ResponseBody
     public HttpJSONResult verifyTmpTicket(
@@ -146,6 +161,30 @@ public class SSOController {
         return HttpJSONResult.ok(JsonUtils.jsonToPojo(redisUserJson, UsersVO.class));
     }
 
+    /**
+     * 验证用户的全局门票
+     * @param userTicket 全局门票
+     * @return boolean
+     */
+    private boolean verifyUserTicket(String userTicket) {
+        if (StringUtils.isBlank(userTicket)) {
+            return false;
+        }
+        String userId = redisUtil.get(REDIS_USER_TICKET_PREFIX + userTicket);
+        if (StringUtils.isBlank(userId)) {
+            return false;
+        }
+        String userInfo = redisUtil.get(REDIS_USER_TOKEN_PREFIX + userId);
+        if (StringUtils.isBlank(userInfo)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 创建临时门票 userTempTicket:tempTicket
+     * @return {@link String}
+     */
     private String createTempTicket() {
         String res = UUID.randomUUID().toString().trim();
         try {
